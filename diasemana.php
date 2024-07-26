69357eabecdffc927700ad71457c8f00b86a40fb
Salvatiico <fernandosalvatiko07@gmail.com>
2024-07-26 16:18:00 -0300

    Add files via upload

---
+++ diasemana.php
@@ -0,0 +1,41 @@
+<?php
+
+$diaNmr=5;
+
+switch ($diaNmr){
+
+    case 1 :
+
+        echo "Hoje é domingo";
+        break ;
+
+    case 2 :
+        echo "Hoje é segunda";
+        break ;
+
+    case 3 :
+        echo "Hoje é terça";
+        break ;
+
+    case 4 :
+        echo "Hoje é quarta";
+        break ;
+
+    case 5 :
+        echo "Hoje é quinta";
+        break ;
+
+    case 6 :
+        echo "Hoje é sexta";
+        break ;
+
+    case 7 :
+        echo "Hoje é sábado";
+        break ;
+
+    default:
+        echo "para de graça fdp";       
+        
+}
+
+?>
