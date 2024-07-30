f5d41e5dac3b20fc6935ad06442de991f96e8522
Salvatiico <fernandosalvatiko07@gmail.com>
2024-07-30 14:44:13 -0300

    Add files via upload

---
+++ increDecreMento.php
@@ -0,0 +1,17 @@
+<?php
+
+//pré encremento
+$pao = 10;
+echo "O valor contido em pão é igual a " .$pao . "<br/>";
+echo "O valor após o incremento é igual a " .++$pao;
+
+echo "<br/>O valor contido em pão é igual a " .$pao . "<br/>";
+echo "O valor após o incremento é igual a " .--$pao;
+
+echo "<br/>O valor contido em pão é igual a " .$pao++ . "<br/>";
+echo "O valor após o incremento é igual a " .$pao;
+
+echo "<br/>O valor contido em pão é igual a " .$pao-- . "<br/>";
+echo "O valor após o incremento é igual a " .$pao;
+
+?>
